575ac730f804a5a5a5dcfc43a1f0ab183b366523
Muhammed Sherif <muhammedsherif40@yahooo.com>
2022-06-25 11:10:05 +0100

    first commit

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+let myName = "Sherif Muhammed"
+
+console.log(myName)
+
+document.write(myName)
+
+let height = "5.8ft"
+
+console.log(height)
+document.write(height)
+
+let country = "Guinea"
+
+console.log(country)
+
+document.write(country)
+
